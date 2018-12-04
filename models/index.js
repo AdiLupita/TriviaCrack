2172556ec0872e55101039c8846ef25d2b9e0d6a
Adriana Castañeda <castalozano.adrigpe@gmail.com>
2018-12-04 02:46:17 -0600

    #21 add category model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 exports.MdlUser = require('./user');
 exports.MdlGame = require('./game');
 exports.MdlQuestion = require('./question');
+exports.MdlCategory = require('./category');
 exports.API = require('./api');
