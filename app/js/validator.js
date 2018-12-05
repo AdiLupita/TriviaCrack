79b884b6ccefd505c9547b5979e9cec5735fe6d0
ricmtz <ricmtz15@gmail.com>
2018-12-05 16:37:39 -0600

    #32 separation of validations, login

--- app/js/validator.js
+++ app/js/validator.js
@@ -61,61 +61,6 @@ function valEmail(data) {
     return (VALITATIONS.email.test(data))
 }
 
-window.addEventListener('load', validateLoginForm);
-
-function apiLogin(nick, pass) {
-    const header = {
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-    const data = { nickname: nick, password: pass };
-    const body = new URLSearchParams(data).toString();
-    API.post('/login', body, header)
-        .then((res) => {
-            if (res.status === 200) {
-                window.location = res.url;
-            } else {
-                msgErrVal('msg-alert-log', 'User/password combination is not valid');
-            }
-        })
-}
-
-function valInputLogin() {
-    const formLogin = document.getElementById('form-login');
-    const nicknameInput = formLogin.elements.namedItem('nickname');
-    const passwordInput = formLogin.elements.namedItem('password');
-    let correct = true;
-    const msg = [];
-    if (!valNickname(nicknameInput.value)) {
-        invalidInput(nicknameInput);
-        msg.push(MSG_INVAL_NICKNAME);
-        correct = false;
-    } else {
-        validInput(nicknameInput);
-    }
-    if (!valPassword(passwordInput.value)) {
-        invalidInput(passwordInput);
-        msg.push(MSG_INVAL_PASSWORD);
-        correct = false;
-    } else {
-        validInput(passwordInput);
-    }
-    if (correct) {
-        msgErrValHide('msg-alert-log');
-        apiLogin(nicknameInput.value, passwordInput.value);
-    } else {
-        msgErrVal('msg-alert-log', msg.join(', '));
-    }
-}
-
-function validateLoginForm() {
-    try {
-        const btnLogin = document.getElementById('btn-login');
-        btnLogin.addEventListener('click', valInputLogin);
-    } catch (error) {
-
-    }
-}
-
 window.addEventListener('load', leaveSession);
 
 function apiLogout() {
